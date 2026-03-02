87674cfb44796944990fe332052767471f0bdfcc
kautsar rahman <kautsar.rahman@gmail.com>
2026-03-02 10:11:02 +0900

    Form intro and setup

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  
+  <signup-form />
+  
+</template>
+
+<script>
+import SignupForm from './components/SignupForm.vue';
+
+export default {
+  name: 'App',
+  components: {
+    SignupForm
+  }
+}
+
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Arial, Helvetica, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: #2c3e50;
+  margin-top: 60px;
+}
+body {
+  margin: 0;
+  background: #eee;
+}
+</style>
