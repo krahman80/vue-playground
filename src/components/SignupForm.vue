c1d00a6d9086a676a05d045128621cbbd646d35e
kautsar rahman <kautsar.rahman@gmail.com>
2026-03-02 15:22:15 +0900

    Submitting the form

--- src/components/SignupForm.vue
+++ src/components/SignupForm.vue
@@ -1,10 +1,11 @@
 <template>
-    <form>
+    <form @submit.prevent="handleSubmit">
         <label>Email:</label>
         <input type="email" required v-model="email">
     
         <label>Password:</label>
         <input type="password" required v-model="password">
+        <div v-if="passwordError" class="error">{{ passwordError }}</div>
 
         <label>Role:</label>
         <select v-model="role">
@@ -12,7 +13,8 @@
             <option value="designer">Web Designer</option>
         </select>
 
-        <label>Skill:</label>
+        <label>Skill: </label>
+        <p class="memo">Use coma (,) after typing a skill</p>
         <input type="text" v-model="tempSkill" @keyup="addSkill">        
         <div v-for="skill in skills" :key="skill" class="pill">
             <span @click="removeSkill(skill)">{{ skill }}</span>
@@ -21,6 +23,10 @@
         <div class="terms">
             <input type="checkbox" v-model="terms" required>
             <label>Accept term and conditions</label>
+        </div>
+
+        <div class="submit">
+            <button >Create an Account</button>
         </div>
 
         <!-- <div>
@@ -53,7 +59,8 @@ export default {
             role: 'designer',
             terms: false,
             tempSkill: '',
-            skills: []
+            skills: [],
+            passwordError: ''
             // names: []
         }
     },
@@ -74,6 +81,18 @@ export default {
             this.skills = this.skills.filter((item) => {
                 return skill !== item
             })
+        },
+        handleSubmit() {
+            // validate password
+            this.passwordError = this.password.length > 5 ? '' : 'Password must be at lease 6 char long'
+
+            if(!this.passwordError) {
+                console.log('email: ', this.email)
+                console.log('password: ', this.password)
+                console.log('role: ', this.role)
+                console.log('skills: ', this.skills)
+                console.log('terms accepted: ', this.terms)
+            }
         }
     }
 }
@@ -124,5 +143,30 @@ form {
     font-weight: bold;
     color: #777;
     cursor: pointer;
+  }
+  button {
+    background: #0b6dff;
+    border: 0;
+    padding: 10px 20px;
+    margin-top: 20px;
+    color: white;
+    border-radius: 20px;
+  }
+  .submit {
+    text-align: center;
+  }
+  .error {
+    color: #ff0062;
+    margin-top: 10px;
+    font-size: 0.8em;
+    font-weight: bold;
+  }
+  .memo {
+    color: #aaa;
+    margin: 10px 0 5px;
+    font-size: 0.6em;
+    text-transform: uppercase;
+    letter-spacing: 1px;
+    font-weight: bold;
   }
 </style>
