814c0a6d29d5412b13d3917573e689a03788138e
kautsar rahman <kautsar.rahman@gmail.com>
2026-03-02 12:44:02 +0900

    Keyboard event and modifiers

--- src/components/SignupForm.vue
+++ src/components/SignupForm.vue
@@ -12,6 +12,12 @@
             <option value="designer">Web Designer</option>
         </select>
 
+        <label>Skill:</label>
+        <input type="text" v-model="tempSkill" @keyup="addSkill">        
+        <div v-for="skill in skills" :key="skill" class="pill">
+            <p>{{ skill }}</p>
+        </div>
+
         <div class="terms">
             <input type="checkbox" v-model="terms" required>
             <label>Accept term and conditions</label>
@@ -46,7 +52,21 @@ export default {
             password: '',
             role: 'designer',
             terms: false,
+            tempSkill: '',
+            skills: []
             // names: []
+        }
+    },
+    methods: {
+        addSkill(e) {
+            if(e.key === ',' && this.tempSkill)
+            {
+                if(!this.skills.includes(this.tempSkill))
+                {
+                    this.skills.push(this.tempSkill)
+                }
+                this.tempSkill = ''
+            }
         }
     }
 }
