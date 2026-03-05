6668d86d83c0bfd87a6e963e5de05c41206cd1a9
kautsar rahman <kautsar.rahman@gmail.com>
2026-03-06 05:18:14 +0900

    Folder structure

---
+++ src/views/jobs/Jobs.vue
@@ -0,0 +1,24 @@
+<template>
+<h1>Jobs</h1>
+<h2 v-for="job in jobs" :key="job.id"> {{ job.title }}</h2>
+
+
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            jobs: [
+                { title: 'ux designer', id: 1, details: "lorem"},
+                { title: 'web developer', id: 2, details: "lorem"},
+                { title: 'vue developer', id: 3, details: "lorem"}
+            ]
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
