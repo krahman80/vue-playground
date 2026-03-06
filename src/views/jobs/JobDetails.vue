7b3b9d7221ab0b6e250b9583fbb62c4948560b5c
kautsar rahman <kautsar.rahman@gmail.com>
2026-03-06 18:34:43 +0900

    Route parameters

---
+++ src/views/jobs/JobDetails.vue
@@ -0,0 +1,18 @@
+<template>
+<h1>Job Details Page</h1>
+job id is {{ id }}
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            id: this.$route.params.id
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
