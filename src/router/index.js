adbe07c1df3128d8a66cce341bf4f67266726d13
kautsar rahman <kautsar.rahman@gmail.com>
2026-03-07 05:11:23 +0900

    404 pages and redirect

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import {createRouter, createWebHistory} from 'vue-router'
 import Home from '../views/Home.vue'
 import About from '../views/About.vue'
 import Jobs from '../views/jobs/Jobs.vue'
+import NotFound from '../views/NotFound.vue'
 import JobDetails from '../views/jobs/JobDetails.vue'
 
 const routes = [
@@ -25,6 +26,17 @@ const routes = [
       name: 'JobDetails',
       component: JobDetails,
       props: true
+    },
+    // redirect
+    {
+      path: '/all-jobs',
+      redirect: 'jobs' 
+    },
+    // catchall 404
+    {
+      path: '/:catchAll(.*)',
+      name: 'NotFound',
+      component: NotFound
     }
   ]
   
