19ca95fbebabc9f0349b4c2adad91a18a4bb5cb7
kautsar rahman <kautsar.rahman@gmail.com>
2026-03-09 14:35:29 +0900

    Programatic navigation

--- src/App.vue
+++ src/App.vue
@@ -6,8 +6,29 @@
     <!-- <router-link to="/about">About</router-link> -->
     <!-- <a href="/">Home (anchor tag)</a> -->
   </div>
+  
+  <button @click="redirect">Redirect</button>
+  <button @click="back">Go back</button>
+  <button @click="forward">Go forward</button>
+
   <router-view/>
 </template>
+
+<script>
+export default {
+  methods: {
+    redirect() {
+      this.$router.push({ name: 'Home'})
+    },
+    back() {
+      this.$router.go(-1)
+    },
+    forward() {
+      this.$router.go
+    }
+  }
+}
+</script>
 
 <style>
 #app {
@@ -35,5 +56,11 @@
 #nav a.router-link-exact-active {
   color: white;
   background: crimson;
+}
+button {
+  margin: 0 10px;
+  padding: 10px;
+  border: none;
+  border-radius: 4px;
 }
 </style>
